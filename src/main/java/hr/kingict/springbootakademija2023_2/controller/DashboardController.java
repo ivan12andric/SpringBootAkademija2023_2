975cc7b3cb0fbb44effb536aec17920414e0264a
Ivan Andrić <ivan.andric@king-ict.hr>
2023-10-21 11:22:20 +0200

    Implementacija dashboarda i profila

--- src/main/java/hr/kingict/springbootakademija2023_2/controller/DashboardController.java
+++ src/main/java/hr/kingict/springbootakademija2023_2/controller/DashboardController.java
@@ -1,13 +1,18 @@
 package hr.kingict.springbootakademija2023_2.controller;
 
+import hr.kingict.springbootakademija2023_2.service.DashboardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardController {
 
+    @Autowired
+    private DashboardService dashboardService;
+
     @GetMapping(value = "/dashboard")
     public String getDashboard(){
-        return "dashboard.html";
+        return dashboardService.getDashboard();
     }
 }
