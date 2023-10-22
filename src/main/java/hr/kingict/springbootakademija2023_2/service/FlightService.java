a2e3c0555855feaec895748e6d3e584f611435fb
Ivan Andrić <ivan.andric@king-ict.hr>
2023-10-22 09:36:12 +0200

    Priprema za implementaciju flight search servisa

--- src/main/java/hr/kingict/springbootakademija2023_2/service/FlightService.java
+++ src/main/java/hr/kingict/springbootakademija2023_2/service/FlightService.java
@@ -3,12 +3,14 @@ package hr.kingict.springbootakademija2023_2.service;
 import com.amadeus.Amadeus;
 import com.amadeus.Params;
 import com.amadeus.referencedata.Locations;
+import com.amadeus.resources.FlightOfferSearch;
 import com.amadeus.resources.Location;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -21,21 +23,43 @@ public class FlightService {
 
     Logger logger = LoggerFactory.getLogger(FlightService.class);
 
-    public List<Location> getAirports(String keyword){
+    public List<Location> getAirports(String keyword) {
 
         Params params = Params
                 .with("subType", Locations.AIRPORT)
                 .and("keyword", keyword);
 
-        try{
+        try {
             Location[] locations = amadeus.referenceData.locations.get(params);
             return Arrays.asList(locations);
-        }catch (Exception e){
+        } catch (Exception e) {
 
             logger.error("getAirports error", e);
             return Collections.emptyList();
         }
+    }
 
+
+    public List<FlightOfferSearch> getFlights(String originLocationCode, String destinationLocationCode, LocalDate departureDate, LocalDate returnDate, Integer adults) {
+
+        Params params = Params
+                .with("originLocationCode", originLocationCode)
+                .and("destinationLocationCode", destinationLocationCode)
+                .and("departureDate", departureDate.toString())
+                .and("returnDate", returnDate.toString())
+                .and("adults", adults)
+                .and("nonStop", true)
+                .and("max", 10);
+
+        try {
+            FlightOfferSearch[] flightOfferSearches = amadeus.shopping.flightOffersSearch.get(params);
+
+            return Arrays.asList(flightOfferSearches);
+        } catch (Exception e) {
+            logger.error("getFlights neka greška: " + e.getMessage(), e);
+
+            return Collections.emptyList();
+        }
 
 
     }
