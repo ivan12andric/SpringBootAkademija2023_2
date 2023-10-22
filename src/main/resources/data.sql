8b2dc5c8b3ad3ca40d9f7a3803378ebc3f6491e6
Ivan Andrić <ivan.andric@king-ict.hr>
2023-10-22 13:44:32 +0200

    Dodani moduli za jpa i h2 bazu, konfiguracija baze i testni bazni objekti

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+create table test
+(
+    id int primary key auto_increment,
+    naziv varchar(50),
+    opis varchar(255)
+);
+
+insert into test(naziv, opis) values ('Iva', 'ne radi joj punjač');
+
+
