1b538319f8d62f70622a97a9675f1f5684f75972
Ivan Andrić <ivan.andric@king-ict.hr>
2023-10-22 11:41:56 +0200

    Implementacija servisa za pretragu letova i mapera

--- src/main/java/hr/kingict/springbootakademija2023_2/controller/FlightController.java
+++ src/main/java/hr/kingict/springbootakademija2023_2/controller/FlightController.java
@@ -1,14 +1,16 @@
 package hr.kingict.springbootakademija2023_2.controller;
 
+import com.amadeus.resources.FlightOfferSearch;
 import com.amadeus.resources.Location;
+import hr.kingict.springbootakademija2023_2.dto.FlightSearchDto;
 import hr.kingict.springbootakademija2023_2.dto.LocationDto;
+import hr.kingict.springbootakademija2023_2.form.FlightSearchForm;
+import hr.kingict.springbootakademija2023_2.mapper.FlightOfferSearchFlightSearchDtoMapper;
 import hr.kingict.springbootakademija2023_2.service.FlightService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,6 +20,9 @@ public class FlightController {
 
     @Autowired
     private FlightService flightService;
+
+    @Autowired
+    private FlightOfferSearchFlightSearchDtoMapper flightSearchDtoMapper;
 
     @GetMapping(value = "/airports/{keyword}")
     public ResponseEntity<List<LocationDto>> getAirports(@PathVariable String keyword){
@@ -31,6 +36,19 @@ public class FlightController {
         return ResponseEntity
                 .ok()
                 .body(listAirportsDto);
+
+    }
+
+    @PostMapping(value = "/flights")
+    public ResponseEntity<List<FlightSearchDto>> getFlights(@RequestBody @Valid FlightSearchForm flightSearchForm){
+
+        List<FlightOfferSearch> flights = flightService.getFlights(flightSearchForm.getOriginLocationCode(), flightSearchForm.getDestinationLocationCode(), flightSearchForm.getDepartureDate(), flightSearchForm.getReturnDate(), flightSearchForm.getAdults());
+
+        return ResponseEntity.ok()
+                .body(flights
+                        .stream()
+                        .map(flightOfferSearch -> flightSearchDtoMapper.map(flightOfferSearch))
+                        .toList());
 
     }
 }
---
+++ src/main/java/hr/kingict/springbootakademija2023_2/dto/FlightSearchDto.java
@@ -0,0 +1,110 @@
+package hr.kingict.springbootakademija2023_2.dto;
+
+public class FlightSearchDto {
+
+
+    //outbound data
+    private String outbundDepartureAirport;
+    private String outboundDepartureDate;
+    private String outboundArrivalAirport;
+    private String outboundArrivalDate;
+
+
+    //inbound data
+    private String inboundDepartureAirport;
+    private String inboundDepartureDate;
+    private String inboundArrivalAirport;
+    private String inboundArrivalDate;
+
+    private String price;
+    private String outboundCarrier;
+    private String inboundCarrier;
+
+    public String getOutbundDepartureAirport() {
+        return outbundDepartureAirport;
+    }
+
+    public void setOutbundDepartureAirport(String outbundDepartureAirport) {
+        this.outbundDepartureAirport = outbundDepartureAirport;
+    }
+
+    public String getOutboundDepartureDate() {
+        return outboundDepartureDate;
+    }
+
+    public void setOutboundDepartureDate(String outboundDepartureDate) {
+        this.outboundDepartureDate = outboundDepartureDate;
+    }
+
+    public String getOutboundArrivalAirport() {
+        return outboundArrivalAirport;
+    }
+
+    public void setOutboundArrivalAirport(String outboundArrivalAirport) {
+        this.outboundArrivalAirport = outboundArrivalAirport;
+    }
+
+    public String getOutboundArrivalDate() {
+        return outboundArrivalDate;
+    }
+
+    public void setOutboundArrivalDate(String outboundArrivalDate) {
+        this.outboundArrivalDate = outboundArrivalDate;
+    }
+
+    public String getInboundDepartureAirport() {
+        return inboundDepartureAirport;
+    }
+
+    public void setInboundDepartureAirport(String inboundDepartureAirport) {
+        this.inboundDepartureAirport = inboundDepartureAirport;
+    }
+
+    public String getInboundDepartureDate() {
+        return inboundDepartureDate;
+    }
+
+    public void setInboundDepartureDate(String inboundDepartureDate) {
+        this.inboundDepartureDate = inboundDepartureDate;
+    }
+
+    public String getInboundArrivalAirport() {
+        return inboundArrivalAirport;
+    }
+
+    public void setInboundArrivalAirport(String inboundArrivalAirport) {
+        this.inboundArrivalAirport = inboundArrivalAirport;
+    }
+
+    public String getInboundArrivalDate() {
+        return inboundArrivalDate;
+    }
+
+    public void setInboundArrivalDate(String inboundArrivalDate) {
+        this.inboundArrivalDate = inboundArrivalDate;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getOutboundCarrier() {
+        return outboundCarrier;
+    }
+
+    public void setOutboundCarrier(String outboundCarrier) {
+        this.outboundCarrier = outboundCarrier;
+    }
+
+    public String getInboundCarrier() {
+        return inboundCarrier;
+    }
+
+    public void setInboundCarrier(String inboundCarrier) {
+        this.inboundCarrier = inboundCarrier;
+    }
+}
